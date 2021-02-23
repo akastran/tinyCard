3f8f9ee31715545925449b61023e4c556a8f13fc
ΑΝΔΡΕΑΣ ΚΑΣΤΡΙΣΙΟΣ <KASTRISIOS.A@nbg.gr>
2021-02-23 08:52:34 +0200

    tinyCard.app Dbcontext

---
+++ app/tinyCard.app/DbContextFactory.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tinyCard.Core.Data;
+using tinyCard.Core.Config.Extensions;
+
+namespace tinyCard.app
+{
+    public class DbContextFactory : IDesignTimeDbContextFactory<CardDbContext>
+    {
+        public CardDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath($"{AppDomain.CurrentDomain.BaseDirectory}")
+                .AddJsonFile("appsettings.json", false)
+                .Build();
+
+            var config = configuration.ReadAppConfiguration();
+
+            var optionsBuilder = new DbContextOptionsBuilder<CardDbContext>();
+
+            optionsBuilder.UseSqlServer(
+                config.tinyCardConnectionString,
+                options => {
+                    options.MigrationsAssembly("tinyCard.app");
+                });
+
+            return new CardDbContext(optionsBuilder.Options);
+        }
+    }
+}
